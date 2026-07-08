085c759ee83db3a32c6041410e117d0f9196464d
HHX111-CMD <hhx111@github.com>
2026-07-08 16:05:02 +0800

    图片改为英文文件名避免URL编码问题

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,12 @@ import searchIndex from '../data/searchIndex';
 
 // 校园实拍图片
 const campusImages = [
-  { src: '/images/campus/%E4%B8%9C%E9%97%A8%EF%BC%88%E8%B6%85%E8%B6%8A%E9%97%A8).jpg', caption: '东门（超越门）' },
-  { src: '/images/campus/%E5%8C%97%E4%B8%80%E9%97%A8.jpg', caption: '北一门' },
-  { src: '/images/campus/%E5%8C%97%E4%BA%8C%E9%97%A8.jpg', caption: '北二门' },
-  { src: '/images/campus/%E5%8C%97%E4%B8%89%E9%97%A8.jpg', caption: '北三门' },
-  { src: '/images/campus/%E5%9B%BE%E4%B9%A6%E9%A6%86.jpg', caption: '图书馆' },
-  { src: '/images/campus/%E6%95%99%E5%AD%A6%E6%A5%BC.jpg', caption: '教学楼' },
+  { src: '/images/campus/gate-east.jpg', caption: '东门（超越门）' },
+  { src: '/images/campus/gate-north1.jpg', caption: '北一门' },
+  { src: '/images/campus/gate-north2.jpg', caption: '北二门' },
+  { src: '/images/campus/gate-north3.jpg', caption: '北三门' },
+  { src: '/images/campus/library.jpg', caption: '图书馆' },
+  { src: '/images/campus/teaching-building.jpg', caption: '教学楼' },
 ];
 
 export default function Home() {
